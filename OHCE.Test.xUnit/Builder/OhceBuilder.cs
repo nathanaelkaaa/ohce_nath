66c888251e2be03222eaf78906d4edfd1ef8e7be
Nathanaël <nathanaelkaaa@gmail.com>
2023-01-31 12:25:14 +0100

    ajout tests

---
+++ OHCE.Test.xUnit/Builder/OhceBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE.Test.xUnit.Builder
+{
+    internal class OhceBuilder
+    {
+
+        public static Ohce Default => new OhceBuilder().Build(Langue.Français,Period.Defaut);
+
+        public Ohce Build(Langue langue, Period périodeJournée) => new Ohce(langue,périodeJournée);
+
+ 
+    }
+}
--- OHCE.Test.xUnit/OhceTest.cs
+++ OHCE.Test.xUnit/OhceTest.cs
@@ -1,10 +1,37 @@
-
+using OHCE;
+using OHCE.Test.xUnit.Builder;
+using Xunit;
 
 namespace OHCE.Test.xUnit;
 
 public class OhceTest
 {
+    [Fact(DisplayName =
+        "QUAND on entre une chaîne de caractères " +
+        "ALORS elle est renvoyée en miroir")]
+    public void MiroirTest()
+    {
+        var ohce = OhceBuilder.Default;
+
+        // QUAND on entre une chaîne de caractère
+        var sortie = ohce.Palindrome("toto");
+
+        // ALORS elle est renvoyée en miroir
+        Assert.Contains("otot", sortie);
+    }
+
+    [Fact(DisplayName =
+        "QUAND on entre un palindrome " +
+        "ALORS elle est renvoyée en miroir et Bien dit est renvoyé")]
+    public void PalindromeTest()
+    {
+        var ohce = new OhceBuilder().Build(Langue.Français, Period.Defaut);
 
 
+        // QUAND on entre une chaîne de caractère
+        var sortie = ohce.Palindrome("kayak");
 
+        // ALORS elle est renvoyée en miroir
+        Assert.Contains("kayak Bien dit !", sortie);
+    }
 }
